b6bfcf342fab65df4abb2114080a68fce9dd8492
Samir <samir.guemri@gmx.fr>
2021-03-29 09:22:14 +0200

    Initial commit

---
+++ src/main/java/edu/samir/subreddit/SubRedditApplication.java
@@ -0,0 +1,13 @@
+package edu.samir.subreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/samir/subreddit/SubRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.samir.subreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
